291ce52f7b07346fd788b274173b15af35e9342f
Angel Neupane <angelneupane83@gmail.com>
2025-04-23 22:03:41 +1000

    assets added

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,68 @@
-import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import SignupPage from "./pages/Signup";
-import LoginPage from "./pages/Login";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import TransactionPage from "./pages/TransactionPage";
-import PrivateRoutes from "./components/PrivateRoutes";
+import { ToastContainer, toast,Bounce} from "react-toastify";
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import DefaultLayout from "./components/layout/DefaultLayout";
+import Dashboard from "./pages/Dashboard";
+import Transaction from "./pages/Transaction";
+import Auth from "./auth/Auth.jsx";
+import { useEffect } from "react";
+import { useUser } from "./context/UserContext.jsx";
+import { autoLogin } from "./utils/users.js";
+import Contact from "./pages/Contact.jsx";
+import Home from "./pages/Home.jsx";
 
 function App() {
-  // state to track the user
+  const { user, setUser } = useUser();
+  useEffect(() => {
+    !user?._id && updateUser();
+  }, [user?._id]);
+
+  const updateUser = async () => {
+    const user = await autoLogin();
+    setUser(user);
+  };
   return (
-    <>
+    <div className="wrapper">
       <Routes>
-        {/* Public routes */}
-        <Route path="/" element={<SignupPage />} />
-        <Route path="signup" element={<SignupPage />} />
-        <Route path="login" element={<LoginPage />} />
-        {/* Private Routes */}
-        <Route
-          path="/transactions"
-          element={
-            <PrivateRoutes>
-              <TransactionPage />
-            </PrivateRoutes>
-          }
-        />
+        <Route path="/" element={<DefaultLayout />}>
+          <Route path="login" element={<Login />}></Route>
+          <Route path="signup" element={<Signup />}></Route>
+          <Route path="contact" element={<Contact />}></Route>
+          <Route index element={<Home />}></Route>
+          <Route
+            path="dashboard"
+            element={
+              <Auth>
+                <Dashboard />
+              </Auth>
+            }
+          ></Route>
+          <Route
+            path="transaction"
+            element={
+              <Auth>
+                <Transaction />
+              </Auth>
+            }
+          ></Route>
+        </Route>
       </Routes>
-      <ToastContainer />
-    </>
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        transition={Bounce}
+      />
+    </div>
   );
 }
 
