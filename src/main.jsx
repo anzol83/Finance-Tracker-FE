9d5483ece0f0271d844a6d7401b3ba6f4193cdb6
Angel Neupane <angelneupane83@gmail.com>
2025-04-23 22:12:08 +1000

    auth and context

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter } from 'react-router';
-import { Provider } from 'react-redux';
-import store from './store.js';
+import { createContext, StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-toastify/dist/ReactToastify.css";
+import { BrowserRouter } from "react-router-dom";
+import { UserProvider } from "./context/UserContext.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Provider store={store}>
+createRoot(document.getElementById("root")).render(
+    <UserProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </Provider>
-  </StrictMode>,
-)
+    </UserProvider>
+);
